5a63798c42cea576ad930166644871ae386a28a8
fragosti <francesco.agosti93@gmail.com>
2018-09-09 01:47:51 +0200

    Add padding for responsive layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,44 +21,46 @@ class App extends React.Component {
     public render() {
         return (
             <BrowserRouter>
-                <Layout>
-                    <Header fixed={false} float={false} size="large" splash={false}>
-                        <Title>
-                            <Link to="/">
-                                <Logo />
-                            </Link>
-                        </Title>
-                        <Box flex={true} justify="end" direction="row" responsive={false}>
-                            <Menu icon={<Actions />} dropAlign={{ right: 'right' }}>
-                                <Anchor>
-                                    <Link to="orders">See Your Orders</Link>
-                                </Anchor>
-                                <Anchor>
-                                    <Link to="orderbook">See All Orders</Link>
-                                </Anchor>
-                                <Anchor>
-                                    <Link to="/">See Token Sets</Link>
-                                </Anchor>
-                            </Menu>
-                        </Box>
-                    </Header>
-                    <Section margin={{ vertical: 'large' }}>
-                        <Route exact={true} path="/" component={SetSelectionPage} />
-                        <Route path="/issue/:setId" component={IssueSetPage} />
-                        <Route path="/otc" component={OTCApp} />
-                    </Section>
-                    <Footer justify="between">
-                        <Title />
-                        <Box direction="row" align="center" pad={{ between: 'medium' }}>
-                            <Paragraph margin="none">© 2018 Bündler</Paragraph>
-                            <Menu direction="row" size="small" dropAlign={{ right: 'right' }}>
-                                <Anchor href="#">About</Anchor>
-                                <Anchor href="#">FAQ</Anchor>
-                                <Anchor href="#">Contact</Anchor>
-                            </Menu>
-                        </Box>
-                    </Footer>
-                </Layout>
+                <Box pad="medium">
+                    <Layout>
+                        <Header fixed={false} float={false} size="large" splash={false}>
+                            <Title>
+                                <Link to="/">
+                                    <Logo />
+                                </Link>
+                            </Title>
+                            <Box flex={true} justify="end" direction="row" responsive={false}>
+                                <Menu icon={<Actions />} dropAlign={{ right: 'right' }}>
+                                    <Anchor>
+                                        <Link to="orders">See Your Orders</Link>
+                                    </Anchor>
+                                    <Anchor>
+                                        <Link to="orderbook">See All Orders</Link>
+                                    </Anchor>
+                                    <Anchor>
+                                        <Link to="/">See Token Sets</Link>
+                                    </Anchor>
+                                </Menu>
+                            </Box>
+                        </Header>
+                        <Section margin={{ vertical: 'large' }}>
+                            <Route exact={true} path="/" component={SetSelectionPage} />
+                            <Route path="/issue/:setId" component={IssueSetPage} />
+                            <Route path="/otc" component={OTCApp} />
+                        </Section>
+                        <Footer justify="between">
+                            <Title />
+                            <Box direction="row" align="center" pad={{ between: 'medium' }}>
+                                <Paragraph margin="none">© 2018 Bündler</Paragraph>
+                                <Menu direction="row" size="small" dropAlign={{ right: 'right' }}>
+                                    <Anchor href="#">About</Anchor>
+                                    <Anchor href="#">FAQ</Anchor>
+                                    <Anchor href="#">Contact</Anchor>
+                                </Menu>
+                            </Box>
+                        </Footer>
+                    </Layout>
+                </Box>
             </BrowserRouter>
         );
     }
